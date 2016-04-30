b00c1378a1260c6dfd5ecb0f0f22e73f8116783a
Isak Glans <isakglans@hotmail.com>
2016-04-30 11:56:51 +0200

    first

---
+++ signalr_v2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="signalr_v2.MvcApplication" Language="C#" %>
---
+++ signalr_v2/Hubs/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace signalr_v2
+{
+    public class ChatHub : Hub
+    {
+        public void Hello(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+        public void RoleDie(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, "Du slog 1555");
+        }
+        public void RoleDie2(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, "Du slog 1555");
+        }
+
+        public override Task OnConnected()
+        {            
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {            
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {            
+            return base.OnReconnected();
+        }
+    }
+}
